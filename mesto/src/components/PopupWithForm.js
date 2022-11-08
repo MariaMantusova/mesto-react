6d7fcc0646bf5af2eb48c95a0196653fe0f19bec
MariaMantusova <maria.mantusova@mail.ru>
2022-11-08 22:19:56 +0300

    feat: add PopupWithForm

---
+++ mesto/src/components/PopupWithForm.js
@@ -0,0 +1,17 @@
+function PopupWithForm(props) {
+    return(
+        <div className={`popup popup_theme_${props.name}`}>
+            <div className="popup__container">
+                <button type="button"
+                        className="popup__button-close"></button>
+                <h2 className="popup__title">{`${props.title}`}</h2>
+                <form className="popup__form" name={`${props.name}`}>
+                    {`${props.children}`}
+                    <button className="popup__button popup__button_theme_edit-photo">
+                        {`${props.buttonText}`}
+                    </button>
+                </form>
+            </div>
+        </div>
+    )
+}
