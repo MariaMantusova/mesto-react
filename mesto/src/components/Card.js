fb0ddbc663a578b3fe639be9a180ebfda74822c0
MariaMantusova <maria.mantusova@mail.ru>
2022-11-25 14:30:26 +0300

    feat: add context in card

--- mesto/src/components/Card.js
+++ mesto/src/components/Card.js
@@ -1,6 +1,12 @@
 import React from "react";
+import {CurrentUserContext} from "../contexts/CurrentUserContext"
 
 function Card(props) {
+    const userInfo = React.useContext(CurrentUserContext);
+    const isOwn = props.card.owner._id === userInfo._id;
+    const isLiked = props.card.likes.some(i => i._id === userInfo._id);
+    const cardDeleteButtonClassName = `card__delete-button ${!isOwn && 'card__delete-button_inactive'}`;
+    const cardLikeButtonClassName = `card__like ${isLiked && 'card__like_active'}`;
 
     function handleClick() {
         props.onCardClick(props.card);
@@ -8,11 +14,11 @@ function Card(props) {
 
     return(
         <article className="card" key={props.card._id}>
-            <button aria-label="удалить" type="button" className="card__delete-button"></button>
+            <button aria-label="удалить" type="button" className={cardDeleteButtonClassName}></button>
             <img className="card__image" src={`${props.card.link}`} alt={`${props.card.name}`} onClick={handleClick} />
             <div className="card__info">
                 <h2 className="card__title">{`${props.card.name}`}</h2>
-                <button type="button" className="card__like"></button>
+                <button type="button" className={cardLikeButtonClassName}></button>
                 <span className="card__like_sum">{`${props.card.likes.length}`}</span>
             </div>
         </article>
