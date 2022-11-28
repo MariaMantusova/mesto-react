d731f24a7e1e22d56ddfa134a6fa70c21c339916
MariaMantusova <maria.mantusova@mail.ru>
2022-11-28 14:06:49 +0300

    fix: change component edit profile

--- mesto/src/components/EditProfilePopup.js
+++ mesto/src/components/EditProfilePopup.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PopupWithForm from "./PopupWithForm";
 import {CurrentUserContext} from "../contexts/CurrentUserContext"
 
 function EditProfilePopup(props) {
@@ -20,27 +21,18 @@ function EditProfilePopup(props) {
     }
 
     return (
-        <div className={`popup popup_theme_${props.name} ${props.isOpen ? 'popup_opened' : ''}`}>
-            <div className="popup__container">
-                <button type="button"
-                        className="popup__button-close" onClick={props.onClose}></button>
-                <h2 className="popup__title">Редактировать профиль</h2>
-                <form className="popup__form" name="profile-info" noValidate>
-                    <input type="text" id="profile-info-input-name" name="name"
-                           className="popup__item popup__item_el_name" value={name || ''} onChange={handleChangeName}
-                           placeholder="Имя пользователя" minLength="2" maxLength="40" required/>
-                    <span className="profile-info-input-name-error popup__item-error"></span>
-                    <input type="text" id="profile-info-input-job" name="job"
-                           className="popup__item popup__item_el_job" value={description || ''}
-                           onChange={handleChangeDescription}
-                           placeholder="Род деятельности" minLength="2" maxLength="200" required/>
-                    <span className="profile-info-input-job-error popup__item-error"></span>
-                    <button className="popup__button popup__button_theme_edit-photo" onSubmit={props.onClose}>
-                        Сохранить
-                    </button>
-                </form>
-            </div>
-        </div>
+        <PopupWithForm name="profile-info" title="Редактировать профиль" buttonText="Сохранить"
+                       isOpen={props.isOpen} onClose={props.onClose}>
+                <input type="text" id="profile-info-input-name" name="name"
+                       className="popup__item popup__item_el_name" value={name || ''} onChange={handleChangeName}
+                       placeholder="Имя пользователя" minLength="2" maxLength="40" required/>
+                <span className="profile-info-input-name-error popup__item-error"></span>
+                <input type="text" id="profile-info-input-job" name="job"
+                       className="popup__item popup__item_el_job" value={description || ''}
+                       onChange={handleChangeDescription}
+                       placeholder="Род деятельности" minLength="2" maxLength="200" required/>
+                <span className="profile-info-input-job-error popup__item-error"></span>
+        </PopupWithForm>
     )
 }
 
